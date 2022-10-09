5bdefbb586c31cd4eedbdc3a3913fd0470bb5f5e
introbond <108642001+introbond@users.noreply.github.com>
2022-10-09 09:33:27 +0700

    Fix sql bug before insert using Insert + select syntax

--- app/server.js
+++ app/server.js
@@ -19,18 +19,17 @@ connection.connect((error) => {
     }
 })
 
-// 3.1
+// Solution for question number 3.1
 app.get(`/read/sales_history/:car_brand/:date_start/:date_end`, async (req, res) => {
     const {car_brand, date_start, date_end} = req.params
     try {
-        connection.query(`SELECT * FROM sales_history WHERE (car_brand = ?) AND date BETWEEN ? AND ?`,
-        [car_brand, date_start, date_end],
+        connection.query(`SELECT * FROM sales_history WHERE (car_brand = '${car_brand}') AND date BETWEEN '${date_start}' AND '${date_end}'`,
         (error, results, fields) => {
             if (error) {
                 console.log(error)
                 return res.status(400).send()
             } else {
-                return res.status(200).json(results)
+                return res.status(200).send(results)
             }
         })
     } catch(error) {
@@ -39,12 +38,11 @@ app.get(`/read/sales_history/:car_brand/:date_start/:date_end`, async (req, res)
     }
 })
 
-//3.2
+// Solution for question number 3.2
 app.get(`/read/sales_history/:car_brand/:car_model/:date_start/:date_end`, async (req, res) => {
     const {car_brand, car_model, date_start, date_end} = req.params
     try {
-        connection.query(`SELECT * FROM sales_history WHERE (car_brand = ?) AND (car_model = ?) AND date BETWEEN ? AND ?`,
-        [car_brand, car_model, date_start, date_end],
+        connection.query(`SELECT * FROM sales_history WHERE (car_brand = '${car_brand}') AND (car_model = '${car_model}') AND date BETWEEN '${date_start}' AND '${date_end}'`,
         (error, results, fields) => {
             if (error) {
                 console.log(error)
@@ -59,12 +57,11 @@ app.get(`/read/sales_history/:car_brand/:car_model/:date_start/:date_end`, async
     }
 })
 
-//3.3
+// Solution for question number 3.3
 app.get(`/read/sales_history/:car_brand/:car_model/:staff_id/:date_start/:date_end`, async (req, res) => {
     const {car_brand, car_model, staff_id, date_start, date_end} = req.params
     try {
-        connection.query(`SELECT * FROM sales_history WHERE (car_brand = ?) AND (car_model = ?) AND (staff_id = ?) AND date BETWEEN ? AND ?`,
-        [car_brand, car_model, staff_id, date_start, date_end],
+        connection.query(`SELECT * FROM sales_history WHERE (car_brand = '${car_brand}') AND (car_model = '${car_model}') AND (staff_id = '${Number(staff_id)}') AND date BETWEEN '${date_start}' AND '${date_end}'`,
         (error, results, fields) => {
             if (error) {
                 console.log(error)
@@ -79,20 +76,23 @@ app.get(`/read/sales_history/:car_brand/:car_model/:staff_id/:date_start/:date_e
     }
 })
 
-//3.4
+// Solution for question number 3.4
 app.post(`/create/sales_history`, async (req, res) => {
     const {staff_id, car_brand, car_model,} = req.body
     try {
         connection.query(
-            `INSERT INTO sales_history(staff_id, car_brand, car_model) VALUES(?, ?, ?)`,
-            [staff_id, car_brand, car_model],
+            `INSERT INTO sales_history (staff_id, car_brand, car_model) VALUES (
+            (SELECT id FROM staffs WHERE id = '${Number(staff_id)}'), 
+            (SELECT brand FROM cars WHERE brand = '${car_brand}' AND model = '${car_model}'),
+            (SELECT model FROM cars WHERE brand = '${car_brand}' AND model = '${car_model}')
+            )`,
             (error, results, fields) => {
                 if (error) {
-                    console.log(`Insert error: `, error)
+                    console.log(error)
                     return res.status(400).send()
                 } else {
-                    return res.status(201).json({ message: `Reccorded success: Staff ID = ${staff_id}, Brand = ${car_brand}, Model = ${car_model}`})
-                }
+                    return res.status(201).json({ message: `Recorded success: Staff ID = ${Number(staff_id)}, Brand = ${car_brand}, Model = ${car_model}`})
+                }   
             }
         )
     } catch(error) {
@@ -101,19 +101,18 @@ app.post(`/create/sales_history`, async (req, res) => {
     }
 })
 
-//3.5
+// Solution for question number 3.5
 app.post(`/create/car_model`, async (req, res) => {
     const {car_brand, car_model, car_description} = req.body
     try {
         connection.query(
-            `INSERT INTO cars(brand, model, description) VALUES(?, ?, ?)`,
-            [car_brand, car_model, car_description],
+            `INSERT INTO cars(brand, model, description) VALUES('${car_brand}', '${car_model}', '${car_description}')`,
             (error, results, fields) => {
                 if (error) {
-                    console.log(`Insert error: `, error)
+                    console.log(error)
                     return res.status(400).send()
                 } else {
-                    return res.status(201).json({ message: `Reccorded success: Car Brand = ${car_brand}, Car Model = ${car_model}, Car Description = ${car_description}`})
+                    return res.status(201).json({ message: `Recorded success: Car Brand = ${car_brand}, Car Model = ${car_model}, Car Description = ${car_description}`})
                 }
             }
         )
@@ -123,12 +122,13 @@ app.post(`/create/car_model`, async (req, res) => {
     }
 })
 
-//3.6
+// Solution for question number 3.6
 app.patch(`/update/car_model`, async (req, res) => {
     const {car_brand, car_model_old, car_model_new} = req.body
     try {
-        connection.query(`UPDATE cars SET model = ? WHERE (brand = ?) AND (model = ?)`, 
-        [car_model_new, car_brand, car_model_old], 
+        connection.query(`UPDATE cars SET model = '${car_model_new}'
+        WHERE (SELECT brand FROM cars WHERE brand = '${car_brand}' AND model = '${car_model_old}') 
+        AND (SELECT model FROM cars WHERE brand = '${car_brand}' AND model = '${car_model_old}')`,
         (error, results, fields) => {
             if (error) {
                     console.log(error)
